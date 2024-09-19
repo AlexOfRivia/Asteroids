d5be581a06006717c03b16e8c35954e1f30ec32d
AlexOfRivia <alexbhandari22@gmail.com>
2024-09-19 02:26:00 +0200

    Changes to Game class

--- Asteroids/Game.cpp
+++ Asteroids/Game.cpp
@@ -1,1 +1,31 @@
 #include "Game.h"
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+
+}
+
+void Game::InitVariables()
+{
+	this->window = nullptr;
+}
+
+void Game::InitWindow()
+{
+	this->window = new sf::RenderWindow(sf::VideoMode(1920, 1080), "Asteroids", sf::Style::Titlebar | sf::Style::Close);
+}
+
+Game::Game()
+{
+	this->InitVariables(); //this function must be first, because of window being set to nullptr
+	this->InitWindow();
+}
+
+Game::~Game()
+{
+	delete this->window;
+}
--- Asteroids/Game.h
+++ Asteroids/Game.h
@@ -1,13 +1,27 @@
 #pragma once
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+
+/*
+This class basically acts as a game engine
+*/
 
 class Game
 {
 	public:
-	
+	//public functions
+	void Update();
+	void Render();
+	Game();
+	~Game();
 
 	private:
-	
+		sf::RenderWindow* window;
+		sf::Event event;
+		void InitVariables();
+		void InitWindow();
 };
 
