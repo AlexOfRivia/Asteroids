51cdfd84210e55fabaa34bd9707bb1efc58684fb
AlexOfRivia <alexbhandari22@gmail.com>
2024-10-01 19:59:46 +0200

    Minor Changes

--- Asteroids/Asteroid.cpp
+++ Asteroids/Asteroid.cpp
@@ -28,17 +28,22 @@ Asteroid::~Asteroid()
 //Initializing asteroid variables
 void Asteroid::initVariavles()
 {
-
+	this->isdestroyed = false;
 }
 
 //Initializing asteroid textures
 void Asteroid::InitTexture()
 {
-
+	//Load Texture from file
+	if (!this->asteroidTexture.loadFromFile("AsteroidSmall/Ship.png"))
+	{
+		std::cout << "ERROR: Unable to load player texture\n";
+	}
 }
 
 //Initializing asteroid sprite
 void Asteroid::InitSprite()
 {
-
+	this->asteroidSprite.setTexture(this->asteroidTexture);
+	this->asteroidSprite.scale(0.10f, 0.10f);
 }
